80da1c09491491d27f7145753b86f0853daf5c6c
Wenzheng Li <wenzhenl@usc.edu>
2016-07-23 12:58:57 -0700

    added launch screen

--- uscfun/AppDelegate.swift
+++ uscfun/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+    for i in 1...300000 {
+      print(i)
+    }
     return true
   }
 
