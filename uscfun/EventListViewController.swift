2d720bd9ee4a0dddbce8ebb4c8e60b14a2903bbb
Wenzheng Li <wenzhenl@usc.edu>
2016-09-03 23:24:40 -0700

    Added button to start an event

--- uscfun/EventListViewController.swift
+++ uscfun/EventListViewController.swift
@@ -15,6 +15,7 @@ class EventListViewController: UIViewController, UITableViewDelegate, UITableVie
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var startEventButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +26,7 @@ class EventListViewController: UIViewController, UITableViewDelegate, UITableVie
         self.rightButton.layer.cornerRadius = rightButton.frame.size.height / 2.0
         self.leftButton.backgroundColor = UIColor.buttonPink()
         self.rightButton.backgroundColor = UIColor.buttonBlue()
+        self.startEventButton.layer.cornerRadius = startEventButton.frame.size.height / 2.0
         self.tableView.backgroundColor = UIColor.backgroundGray()
 //        self.backgroundView.layer.cornerRadius = 15
         self.backgroundView.layer.borderWidth = 1
@@ -33,7 +35,10 @@ class EventListViewController: UIViewController, UITableViewDelegate, UITableVie
         self.leftButton.layer.borderColor = UIColor.lightGrayColor().CGColor
         self.rightButton.layer.borderWidth = 1
         self.rightButton.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.startEventButton.layer.borderWidth = 1
+        self.startEventButton.layer.borderColor = UIColor.buttonBlue().CGColor
         self.view.bringSubviewToFront(buttonContainerView)
+        self.view.bringSubviewToFront(startEventButton)
     }
 
     override func didReceiveMemoryWarning() {
