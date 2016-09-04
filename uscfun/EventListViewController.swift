8966436c1a3ad8fc1aa3b3b8e32739ef263d9b27
Wenzheng Li <wenzhenl@usc.edu>
2016-09-03 21:11:19 -0700

    EventList scene, two buttons added

--- uscfun/EventListViewController.swift
+++ uscfun/EventListViewController.swift
@@ -10,26 +10,25 @@ import UIKit
 
 class EventListViewController: UIViewController {
 
+    @IBOutlet weak var buttonContainerView: UIView!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.themeYellow()
+        self.leftButton.layer.cornerRadius = leftButton.frame.size.height / 2.0
+        self.rightButton.layer.cornerRadius = rightButton.frame.size.height / 2.0
+        self.leftButton.backgroundColor = UIColor.buttonPink()
+        self.rightButton.backgroundColor = UIColor.buttonBlue()
+        self.tableView.backgroundColor = UIColor.backgroundGray()
+        self.tableView.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
