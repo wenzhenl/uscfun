db1b99d51bb5d3117b2b74ed841657753b0494ea
Wenzheng Li <wenzhenl@usc.edu>
2016-09-04 16:31:00 -0700

    UserProfile scene: design with hanging view

--- uscfun/MeViewController.swift
+++ uscfun/MeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MeViewController: UIViewController {
+class MeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -16,25 +16,69 @@ class MeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.navigationController!.navigationBar.barTintColor = UIColor.buttonPink()
+        self.navigationController!.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.lightGrayColor(), NSFontAttributeName: UIFont.systemFontOfSize(20)]
+        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+        self.navigationController!.navigationBar.shadowImage = UIImage()
+        self.view.backgroundColor = UIColor.buttonPink()
+        self.tableView.layer.borderWidth = 2
+        self.tableView.layer.borderColor = UIColor.lightGrayColor().CGColor
+        self.tableView.layer.cornerRadius = 13
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-}
-
-extension MeViewController: UITableViewDataSource {
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 4
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 1
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("UserProfileCell") as! UserProfileTableViewCell
+            return cell
+        }
+        
         return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 13
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return UITableViewAutomaticDimension
+        } else {
+            return 100
+        }
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return UITableViewAutomaticDimension
+        } else {
+            return 100
+        }
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.clearColor()
+    }
+    
+    func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor.lightGrayColor()
+    }
+    
+    @IBAction func close() {
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
 }
---
+++ uscfun/UserProfileTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  UserProfileTableViewCell.swift
+//  uscfun
+//
+//  Created by Wenzheng Li on 9/4/16.
+//  Copyright © 2016 Wenzheng Li. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var avatarButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        avatarButton.layer.cornerRadius = avatarButton.frame.size.height / 2.0
+        avatarButton.backgroundColor = UIColor.buttonBlue()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
