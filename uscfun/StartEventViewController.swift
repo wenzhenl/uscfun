0f707678cc2d5df1e1274a1872c396654f3dca55
Wenzheng Li <wenzhenl@usc.edu>
2016-09-08 22:30:30 -0700

    StartEventScene: Added event title

--- uscfun/StartEventViewController.swift
+++ uscfun/StartEventViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class StartEventViewController: UIViewController {
+class StartEventViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let eventTitleTextViewTag = 1
+    let eventTitleTextViewPlaceHolder = "活动名称"
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +21,9 @@ class StartEventViewController: UIViewController {
         self.navigationController!.navigationBar.barTintColor = UIColor.buttonBlue()
         self.navigationController!.navigationBar.tintColor = UIColor.whiteColor()
         self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont.systemFontOfSize(20)]
+        self.view.backgroundColor = UIColor.backgroundGray()
+        self.tableView.backgroundColor = UIColor.backgroundGray()
+        self.tableView.contentInset = UIEdgeInsetsMake(40, 0, 0, 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,14 +35,103 @@ class StartEventViewController: UIViewController {
         self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
     }
-    */
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("EventTitleCell") as! EventTitleTableViewCell
+        cell.textView.delegate = self
+        cell.textView.tag = eventTitleTextViewTag
+        cell.textView.text = eventTitleTextViewPlaceHolder
+        cell.textView.textColor = UIColor.lightGrayColor()
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.clearColor()
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 15
+    }
+    
+    
+    func tableView(tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+        view.tintColor = UIColor.clearColor()
+    }
 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: false)
+    }
+    
+    // MARK: - TextView delegate
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        if textView.textColor == UIColor.lightGrayColor() {
+            textView.text = nil
+            textView.textColor = UIColor.darkGrayColor()
+        }
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = eventTitleTextViewPlaceHolder
+            textView.textColor = UIColor.lightGrayColor()
+        }
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        
+        // Combine the textView text and the replacement text to
+        // create the updated text string
+        let currentText: NSString = textView.text
+        let updatedText = currentText.stringByReplacingCharactersInRange(range, withString:text)
+        
+        // If updated text view will be empty, add the placeholder
+        // and set the cursor to the beginning of the text view
+        if updatedText.isEmpty {
+            
+            textView.text = eventTitleTextViewPlaceHolder
+            textView.textColor = UIColor.lightGrayColor()
+            
+            textView.selectedTextRange = textView.textRangeFromPosition(textView.beginningOfDocument, toPosition: textView.beginningOfDocument)
+            
+            return false
+        }
+            
+            // Else if the text view's placeholder is showing and the
+            // length of the replacement string is greater than 0, clear
+            // the text view and set its color to black to prepare for
+            // the user's entry
+        else if textView.textColor == UIColor.lightGrayColor() && !text.isEmpty {
+            textView.text = nil
+            textView.textColor = UIColor.blackColor()
+        }
+        
+        return true
+    }
+    
+//    func textViewDidChangeSelection(textView: UITextView) {
+//        if textView.textColor == UIColor.lightGrayColor() {
+//            textView.selectedTextRange = textView.textRangeFromPosition(textView.beginningOfDocument, toPosition: textView.beginningOfDocument)
+//        }
+//    }
 }
