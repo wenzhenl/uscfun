67fc920d5ae6748b44d763b495d65d82956ff738
Wenzheng Li <wenzhenl@usc.edu>
2016-09-27 00:08:36 -0700

    refactor code, use enum for table view cell types

--- uscfun/EventDetailViewController.swift
+++ uscfun/EventDetailViewController.swift
@@ -8,9 +8,20 @@
 
 import UIKit
 
+enum EventDetailCell {
+    case imageViewTableCell(image: UIImage)
+    case textViewTableCell(text: String)
+    case singleButtonTableCell
+    case imgKeyValueTableCell(image: UIImage, key: String, value: String)
+}
+
 class EventDetailViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var detailCells = [[EventDetailCell]]()
+    
+    let eventLocationKey = "活动地点"
+    let mapSegueIdentifier = "SHOWMAP"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +40,17 @@ class EventDetailViewController: UIViewController {
         self.view.backgroundColor = UIColor.backgroundGray()
         self.tableView.backgroundColor = UIColor.backgroundGray()
         self.tableView.tableFooterView = UIView()
+        self.tableView.contentInset = UIEdgeInsetsMake(10, 0, 0, 0)
+        
+        // Populate the cells
+        let firstSection = [EventDetailCell.imageViewTableCell(image: #imageLiteral(resourceName: "add-3")), .textViewTableCell(text: "Happy birthday to Jing Li")]
+        let secondSection = [EventDetailCell.singleButtonTableCell]
+        let thirdSection = [EventDetailCell.imgKeyValueTableCell(image: #imageLiteral(resourceName: "location"), key: "参与讨论", value: ">")]
+        let forthSection = [EventDetailCell.imgKeyValueTableCell(image: #imageLiteral(resourceName: "alarm-clock"), key: "活动时间", value: "星期天上午"), .imgKeyValueTableCell(image: #imageLiteral(resourceName: "paper-plane-1"), key: eventLocationKey, value: "中国城大华超市")]
+        detailCells.append(firstSection)
+        detailCells.append(secondSection)
+        detailCells.append(thirdSection)
+        detailCells.append(forthSection)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -49,37 +71,34 @@ class EventDetailViewController: UIViewController {
 extension EventDetailViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return detailCells.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return detailCells[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        if indexPath.row == 0 {
+        switch detailCells[indexPath.section][indexPath.row] {
+        case .imageViewTableCell(let image):
             let cell = Bundle.main.loadNibNamed("ImageViewTableViewCell", owner: self, options: nil)?.first as! ImageViewTableViewCell
-            cell.mainImageView.image = #imageLiteral(resourceName: "calendar-6")
+            cell.mainImageView.image = image
             return cell
-        }
-            
-        else if indexPath.row == 1 {
+        case .imgKeyValueTableCell(let image, let key, let value):
             let cell = Bundle.main.loadNibNamed("ImgKeyValueTableViewCell", owner: self, options: nil)?.first as! ImgKeyValueTableViewCell
-            cell.mainImageView.image = #imageLiteral(resourceName: "car")
-            cell.keyLabel.text = "活动地点"
-            cell.valueLabel.text = "待定"
+            cell.mainImageView.image = image
+            cell.keyLabel.text = key
+            cell.valueLabel.text = value
             return cell
-        }
-            
-        else if indexPath.row > 1 {
+        case .singleButtonTableCell:
+            let cell = Bundle.main.loadNibNamed("SingleButtonTableViewCell", owner: self, options: nil)?.first as! SingleButtonTableViewCell
+            cell.button.layer.cornerRadius = 13
+            cell.button.setTitle("报名参加", for: .normal)
+            return cell
+        case .textViewTableCell(let text):
             let cell = Bundle.main.loadNibNamed("TextViewTableViewCell", owner: self, options: nil)?.first as! TextViewTableViewCell
-            cell.textView.text = "周末去大华活动及附加按附件是打发；就；收到了非计算机打发时间爱上的解决类似的方式"
+            cell.textView.text = text
             return cell
-        }
-            
-        else {
-            return UITableViewCell()
         }
     }
 }
@@ -87,32 +106,49 @@ extension EventDetailViewController: UITableViewDataSource {
 extension EventDetailViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
+        switch detailCells[indexPath.section][indexPath.row] {
+        case .imageViewTableCell(_):
             return 200
+        default:
+            return UITableViewAutomaticDimension
         }
-        return UITableViewAutomaticDimension
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
+        switch detailCells[indexPath.section][indexPath.row] {
+        case .imageViewTableCell(_):
             return 200
+        default:
+            return UITableViewAutomaticDimension
         }
-        
-        return UITableViewAutomaticDimension
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cell.backgroundColor = UIColor.white
+        switch detailCells[indexPath.section][indexPath.row] {
+        case .imageViewTableCell(_), .singleButtonTableCell:
+            cell.backgroundColor = UIColor.clear
+        default:
+            cell.backgroundColor = UIColor.white
+        }
     }
     
     func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
         view.tintColor = UIColor.clear
     }
     
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if indexPath.row == 1 {
-            performSegue(withIdentifier: "SHOWMAP", sender: self)
+        switch detailCells[indexPath.section][indexPath.row] {
+        case .imgKeyValueTableCell(_, let key, _):
+            if key == eventLocationKey {
+                performSegue(withIdentifier: mapSegueIdentifier, sender: self)
+            }
+        default:
+            break
         }
         
         tableView.deselectRow(at: indexPath, animated: false)
---
+++ uscfun/SingleButtonTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SingleButtonTableViewCell.swift
+//  uscfun
+//
+//  Created by Wenzheng Li on 9/26/16.
+//  Copyright © 2016 Wenzheng Li. All rights reserved.
+//
+
+import UIKit
+
+class SingleButtonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var button: UIButton!
+    
+}
