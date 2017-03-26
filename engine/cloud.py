06476e30eb0ac63f4e19c8712befcb017a8f7aa6
Wenzheng Li <wenzhenl@usc.edu>
2017-03-26 20:30:06 +0800

    added after save and after update hook

--- engine/cloud.py
+++ engine/cloud.py
@@ -14,6 +14,8 @@ from string import digits
 from random import choice
 
 import datetime
+import requests
+import json
 
 engine = Engine(app)
 
@@ -65,9 +67,10 @@ def requestConfirmationCode(**params):
             body = ""
             body += "同学你好!\n\n你正在注册使用USC日常，你的验证码是 "
             body += code
-            body += ".\n此验证码将会在20分钟后失效，请尽快"
-            body += "验证。\n如果你并没有注册USC日常，请忽略此邮件。\n"
-            body += "\n\nUSC日常APP"
+            body += ".\n此验证码将会在20分钟后失效，请尽快验证。\n\n\n"
+            body += "\n如果你并没有注册USC日常，请忽略此邮件。\n"
+            body += "\n\nbest,"
+            body += "\nUSC日常APP"
             message.attach(MIMEText(body, 'plain'))
 
             server = smtplib.SMTP('smtp.gmail.com', '587')
@@ -127,10 +130,10 @@ def receiveFeedback(**params):
             message = MIMEMultipart()
             message['From'] = fromaddr
             message['To'] = toaddr
-            message['Subject'] = "【USC日常】你的注册验证码是 "
-            # body = "反馈来自" + email
-            body = feedback
-            message.attach(MIMEText(body, 'plain'))
+            message['Subject'] = "来自用户的反馈"
+
+            message.attach(MIMEText(email.encode('utf-8'), 'plain', 'utf-8'))
+            message.attach(MIMEText(feedback.encode('utf-8'), 'plain', 'utf-8'))
 
             server = smtplib.SMTP('smtp.gmail.com', '587')
             server.ehlo()
@@ -145,10 +148,30 @@ def receiveFeedback(**params):
     else:
         raise LeanEngineError('邮箱反馈都不能为空')
 
-@engine.before_save('Todo')
-def before_todo_save(todo):
-    content = todo.get('content')
-    if not content:
-        raise LeanEngineError('内容不能为空')
-    if len(content) >= 240:
-        todo.set('content', content[:240] + ' ...')
+@engine.after_save('Event')
+def after_event_save(event):
+    eventId = event.get('objectId')
+    headers = {'Content-Type': 'application/json', \
+        'X-LC-Id': '0ddsmQXAJt5gVLLE604DtE4U-gzGzoHsz', \
+        'X-LC-Key': '86bDxHaspqbCjqxWm53txUxb,master'}
+    url = 'https://api.leancloud.cn/1.1/rtm/broadcast'
+    data = {"from_peer": "sys", \
+            "message": "{\"_lctype\":-1,\"_lctext\":\"new event\", \
+            \"_lcattrs\":{\"reason\": \"new\", \
+            \"eventId\": \"" + eventId + "\"}}", \
+             "conv_id": "58d5288eda2f600064f2c605"}
+    requests.post(url, data=json.dumps(data), headers=headers)
+
+@engine.after_update('Event')
+def after_event_update(event):
+    eventId = event.get('objectId')
+    headers = {'Content-Type': 'application/json', \
+        'X-LC-Id': '0ddsmQXAJt5gVLLE604DtE4U-gzGzoHsz', \
+        'X-LC-Key': '86bDxHaspqbCjqxWm53txUxb,master'}
+    url = 'https://api.leancloud.cn/1.1/rtm/broadcast'
+    data = {"from_peer": "sys", \
+            "message": "{\"_lctype\":-1,\"_lctext\":\"new event\", \
+            \"_lcattrs\":{\"reason\": \"updated\", \
+            \"eventId\": \"" + eventId + "\"}}", \
+             "conv_id": "58d5288eda2f600064f2c605"}
+    requests.post(url, data=json.dumps(data), headers=headers)
