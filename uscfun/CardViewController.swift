807daaefbd8ef9142503fdde5b472fe89a16f880
Wenzheng Li <wenzhenl@usc.edu>
2017-04-11 15:40:10 +0800

    adding welcome page for new user

---
+++ uscfun/CardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CardViewController.swift
+//  uscfun
+//
+//  Created by Wenzheng Li on 4/11/17.
+//  Copyright © 2017 Wenzheng Li. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    var pageIndex: Int?
+    var welcomeCard: WelcomeCard?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ uscfun/PageViewController.swift
@@ -0,0 +1,79 @@
+//
+//  PageViewController.swift
+//  uscfun
+//
+//  Created by Wenzheng Li on 4/11/17.
+//  Copyright © 2017 Wenzheng Li. All rights reserved.
+//
+
+import UIKit
+
+protocol ViewControllerProvider {
+    var initialViewController: UIViewController { get }
+    func viewControllerAtIndex(_ index: Int) -> UIViewController?
+}
+
+struct WelcomeCard {
+    var image: UIImage
+    var note: String
+}
+
+class PageViewController: UIPageViewController {
+    var welcomeCards = [WelcomeCard]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+        setViewControllers([initialViewController], direction: .forward, animated: false, completion: nil)
+
+    }
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        if let viewController = viewController as? CardViewController, let pageIndex = viewController.pageIndex, pageIndex > 0 {
+            return viewControllerAtIndex(pageIndex - 1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        if let viewController = viewController as? CardViewController, let pageIndex = viewController.pageIndex, pageIndex < welcomeCards.count - 1 {
+            return viewControllerAtIndex(pageIndex + 1)
+        }
+        
+        return nil
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return welcomeCards.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+}
+
+// MARK: View controller provider
+
+extension PageViewController: ViewControllerProvider {
+    
+    var initialViewController: UIViewController {
+        return viewControllerAtIndex(0)!
+    }
+    
+    func viewControllerAtIndex(_ index: Int) -> UIViewController? {
+        
+        if let cardViewController = UIStoryboard(name: "Welcome", bundle: nil).instantiateViewController(withIdentifier: "card view controller") as? CardViewController {
+            
+            cardViewController.pageIndex = index
+            cardViewController.welcomeCard = welcomeCards[index]
+            
+            return cardViewController
+        }
+        
+        return nil
+    }
+}
